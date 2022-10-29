a3a5404428dccc4099b41fd2916b840b714312d2
Obi Kastanya <obi.kastanya@sigma-tech.co.id>
2022-10-29 16:03:00 +0700

    delete files in google drive

--- main.py
+++ main.py
@@ -1,41 +1,21 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 from g_drive_service import GoogleDriveService
 
 app=Flask(__name__)
 service=GoogleDriveService().build()
 
-from io import BytesIO
-from googleapiclient.http import MediaIoBaseUpload
-from datetime import datetime
+from apiclient import errors
 
-@app.post('/upload')
-def upload_file():
-    
-    uploaded_file=request.files.get("file")
+@app.delete('/file/<file_id>/')
+def delete_file(file_id):
+    try:
+        service.files().delete(fileId=file_id).execute()
+        return {"status":"OK"}
+    except errors.HttpError as error:
+        return {"status":"Fail", "error_message":error.reason}
+    except Exception as e:
+        return {"status":"Fail", "error_message":str(e)}
+     
 
-    buffer_memory=BytesIO()
-    uploaded_file.save(buffer_memory)
-
-    media_body=MediaIoBaseUpload(uploaded_file, uploaded_file.mimetype, resumable=True)
-    created_at= datetime.now().strftime("%Y%m%d%H%M%S")
-    file_metadata={
-        "name":f"{uploaded_file.filename} ({created_at})",
-        "appProperties":{
-            "file_description":request.form.get("description")
-        }
-    }
-
-    returned_fields="id, name, mimeType, webViewLink, exportLinks, appProperties"
-    
-    upload_response=service.files().create(
-        body = file_metadata, 
-        media_body=media_body,  
-        fields=returned_fields
-    ).execute()
-
-    return upload_response
-
-
- 
 if __name__=='__main__':
     app.run(debug=True, port=8000)
